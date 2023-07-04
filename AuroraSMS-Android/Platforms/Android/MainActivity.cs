a516b13c4f69663e34cad3724061592198a38d36
Topias Mariapori <topias@mariapori.fi>
2023-07-04 16:16:32 +0300

    Solutionin muutokset

--- AuroraSMS-Android/Platforms/Android/MainActivity.cs
+++ AuroraSMS-Android/Platforms/Android/MainActivity.cs
@@ -2,7 +2,7 @@
 using Android.Content.PM;
 using Android.OS;
 
-namespace AuroraSMS_Android
+namespace AuroraSMS_Android.Platforms.Android
 {
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
--- AuroraSMS-Android/Platforms/Android/MainApplication.cs
+++ AuroraSMS-Android/Platforms/Android/MainApplication.cs
@@ -1,12 +1,12 @@
 ﻿using Android.App;
 using Android.Runtime;
 
-namespace AuroraSMS_Android
+namespace AuroraSMS_Android.Platforms.Android
 {
     [Application]
     public class MainApplication : MauiApplication
     {
-        public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+        public MainApplication(nint handle, JniHandleOwnership ownership)
             : base(handle, ownership)
         {
         }
